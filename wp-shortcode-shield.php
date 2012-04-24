4e95cf6642923f6480a9a81d46ce297e447e2afb
vicchi <gary@vicchi.org>
2012-04-24 13:49:05 +0200

    First code commit.

---
+++ wp-shortcode-shield.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Plugin Name: WP ShortCode Shield
+Plugin URI: http://www.vicchi.org/codeage/wp-shortcode-shield/
+Description: Allows posts and pages to easily document WordPress short-codes without the short-code being expanded.
+Version: 1.0
+Author: Gary Gale
+Author URI: http://www.garygale.com/
+License: GPL2
+*/
+
+class WP_ShortCodeShield extends WP_PluginBase {
+	static $instance;
+	
+	const LANGB = '&#91;';
+	const RANGB = '&#93;';
+	
+	function __construct () {
+		self::$instance = $this;
+		
+		$this->hook ('plugins_loaded');
+	}
+	
+	function plugins_loaded () {
+		add_shortcode ('wp_scs', array ($this, 'shortcode'));
+		add_shortcode ('wp_shortcode_shield', array ($this, 'shortcode'));
+	}
+	
+	function shortcode ($atts, $content=null) {
+		extract (shortcode_atts (array ('code' => ''), $atts));
+		
+		// Self-closing shortcode form ...
+		if (empty ($content) && !empty ($code)) {
+			return $this->enclose ($code);
+		}
+		
+		// Enclosing shortcode form ...
+		else {
+			if (strpos ($content, '[') === false) {
+				return $this->enclose ($content);
+			}
+			
+			else {
+				return $content;
+			}
+		}
+	}
+	
+	function enclose ($content) {
+		return self::LANGB . $content . self::RANGB;
+	}
+}	// end-class WP_ShortCodeShield
+
+$__wp_shortcode_shield_instance = new WP_ShortCodeShield;
+
+?>
