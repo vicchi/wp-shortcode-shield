bdcd24d59d2d7cb1d3fdf91d6257c6308eb62f9a
vicchi <gary@vicchi.org>
2012-11-11 06:49:19 +0000

    Ensure WP_ShortCodeShield class is not already defined.

--- wp-shortcode-shield.php
+++ wp-shortcode-shield.php
@@ -13,47 +13,49 @@ define ('WPSHORTCODESHIELD_PATH', plugin_dir_path (__FILE__));
 
 require_once (WPSHORTCODESHIELD_PATH . '/includes/wp-plugin-base/wp-plugin-base.php');
 
-class WP_ShortCodeShield extends WP_PluginBase_v1_1 {
-	static $instance;
+if (!class_exists ('WP_ShortCodeShield')) {
+	class WP_ShortCodeShield extends WP_PluginBase_v1_1 {
+		static $instance;
 	
-	const LANGB = '&#91;';
-	const RANGB = '&#93;';
+		const LANGB = '&#91;';
+		const RANGB = '&#93;';
 	
-	function __construct () {
-		self::$instance = $this;
+		function __construct () {
+			self::$instance = $this;
 		
-		$this->hook ('plugins_loaded');
-	}
-	
-	function plugins_loaded () {
-		add_shortcode ('wp_scs', array ($this, 'shortcode'));
-		add_shortcode ('wp_shortcode_shield', array ($this, 'shortcode'));
-	}
-	
-	function shortcode ($atts, $content=null) {
-		extract (shortcode_atts (array ('code' => ''), $atts));
-		
-		// Self-closing shortcode form ...
-		if (empty ($content) && !empty ($code)) {
-			return $this->enclose ($code);
+			$this->hook ('plugins_loaded');
 		}
+	
+		function plugins_loaded () {
+			add_shortcode ('wp_scs', array ($this, 'shortcode'));
+			add_shortcode ('wp_shortcode_shield', array ($this, 'shortcode'));
+		}
+	
+		function shortcode ($atts, $content=null) {
+			extract (shortcode_atts (array ('code' => ''), $atts));
 		
-		// Enclosing shortcode form ...
-		else {
-			if (strpos ($content, '[') === false) {
-				return $this->enclose ($content);
+			// Self-closing shortcode form ...
+			if (empty ($content) && !empty ($code)) {
+				return $this->enclose ($code);
 			}
-			
+		
+			// Enclosing shortcode form ...
 			else {
-				return $content;
+				if (strpos ($content, '[') === false) {
+					return $this->enclose ($content);
+				}
+			
+				else {
+					return $content;
+				}
 			}
 		}
-	}
 	
-	function enclose ($content) {
-		return self::LANGB . $content . self::RANGB;
-	}
-}	// end-class WP_ShortCodeShield
+		function enclose ($content) {
+			return self::LANGB . $content . self::RANGB;
+		}
+	}	// end-class WP_ShortCodeShield
+}	// if (!class_exists ('WP_ShortCodeShield'))
 
 $__wp_shortcode_shield_instance = new WP_ShortCodeShield;
 
